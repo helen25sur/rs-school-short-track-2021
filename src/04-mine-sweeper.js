d63a576f80bbe388271a2d71d794dc6c7590b30c
Elena Surilova <elen21surilova@gmail.com>
2021-04-12 18:46:06 +0300

    feat: add my solution for the task mine sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,67 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i].push(matrix[i][j]);
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const length = matrix.length - 1;
+
+      const current = matrix[i][j];
+      const prevX = j > 0 ? matrix[i][j - 1] : undefined;
+      const nextX = j < matrix[i].length ? matrix[i][j + 1] : undefined;
+      const prevY = i > 0 ? matrix[i - 1][j] : undefined;
+      const nextY = i < length ? matrix[i + 1][j] : undefined;
+
+      const diagonalPrevX = i > 0 && j > 0 ? matrix[i - 1][j - 1] : undefined;
+      const diagonalNextX = i > 0 && j < matrix[i].length ? matrix[i - 1][j + 1] : undefined;
+      const diagonalPrevY = i < length && j > 0 ? matrix[i + 1][j - 1] : undefined;
+      const diagonalNextY = i < length && j < matrix[i].length ? matrix[i + 1][j + 1] : undefined;
+
+      if (current === false) {
+        result[i][j] = 0;
+      }
+
+      if (prevX === true) {
+        result[i][j] += 1;
+      }
+      if (prevY === true) {
+        result[i][j] += 1;
+      }
+      if (nextX === true) {
+        result[i][j] += 1;
+      }
+      if (nextY === true) {
+        result[i][j] += 1;
+      }
+
+      if (diagonalPrevX === true) {
+        result[i][j] += 1;
+      }
+      if (diagonalNextX === true) {
+        result[i][j] += 1;
+      }
+      if (diagonalPrevY === true) {
+        result[i][j] += 1;
+      }
+      if (diagonalNextY === true) {
+        result[i][j] += 1;
+      }
+
+      if (current === true) {
+        result[i][j] = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
